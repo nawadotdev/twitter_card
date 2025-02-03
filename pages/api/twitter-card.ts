badbfe5a67927d4e9aca9bc67caf15cefb966afa
devnawa <148815756+devnawa@users.noreply.github.com>
2025-02-03 11:54:09 +0300

    f

--- pages/api/twitter-card.ts
+++ pages/api/twitter-card.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { image, redirectUrl } = req.query;
+    const { image, redirectUrl, title, description } = req.query;
 
-    const imageUrl = (typeof image === "string" && image.startsWith("http")) ? image : null
+    const imageUrl = (typeof image === "string" && image.startsWith("http")) ? image : null;
 
     if (!imageUrl) {
         return res.status(400).json({ error: "Invalid image URL" });
@@ -13,8 +13,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         ? redirectUrl
         : null;
 
+    const dynamicTitle = title || "@nawa.dev Twitter Card";
+    const dynamicDescription = description || "Dynamic image for Twitter preview. Powered by @nawa.dev.";
+
     res.setHeader("Content-Type", "text/html");
-    
+
     res.status(200).send(`
         <!DOCTYPE html>
         <html lang="en">
@@ -22,17 +25,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="twitter:card" content="summary_large_image">
-            <meta name="twitter:title" content="Dinamik Twitter Resmi">
-            <meta name="twitter:description" content="Bu resim dinamik olarak değişiyor.">
+            <meta name="twitter:title" content="${dynamicTitle}">
+            <meta name="twitter:description" content="${dynamicDescription}">
             <meta name="twitter:image" content="${imageUrl}">
-            <meta property="og:title" content="Dinamik Resim">
-            <meta property="og:description" content="Bu sayfa Twitter ve diğer platformlarda önizleme oluşturuyor.">
+            <meta property="og:title" content="${dynamicTitle}">
+            <meta property="og:description" content="${dynamicDescription}">
             <meta property="og:image" content="${imageUrl}">
             <meta property="og:type" content="website">
-            <title>Twitter Image Sharing</title>
+            <title>${dynamicTitle}</title>
         </head>
         <body>
-            <h1>Twitter Image</h1>
+            <h1>${dynamicTitle}</h1>
             <img src="${imageUrl}" alt="Image">
             ${redirectTo ? `<script>window.location.href = "${redirectTo}";</script>` : ''}
         </body>
